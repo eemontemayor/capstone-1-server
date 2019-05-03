6ecfdb367d368e53b979797697e9768f5dacb4d4
Enrique Montemayor <eemontemayor@live.com>
2019-05-03 18:42:12 -0500

    x

--- migrations/001.do.create_capstone1_meals.sql
+++ migrations/001.do.create_capstone1_meals.sql
@@ -1,3 +1,13 @@
+CREATE TABLE capstone1_users (
+  id SERIAL PRIMARY KEY,
+  user_name TEXT NOT NULL UNIQUE,
+  full_name TEXT NOT NULL,
+  password TEXT NOT NULL,
+  nickname TEXT,
+  date_created TIMESTAMP NOT NULL DEFAULT now(),
+  date_modified TIMESTAMP
+);
+--- RUN MIGRATE AGAIN AND DELETE 002
 CREATE TABLE capstone1_meals (
   id SERIAL PRIMARY KEY,
   image TEXT,
@@ -8,6 +18,8 @@ CREATE TABLE capstone1_meals (
   bookmarked BOOLEAN
 );
 
---CREATE TABLE capstone1_calendar (
 
---)
+ALTER TABLE capstone1_meals
+  ADD COLUMN
+    user_id INTEGER REFERENCES capstone1_users(id)
+    ON DELETE SET NULL;
