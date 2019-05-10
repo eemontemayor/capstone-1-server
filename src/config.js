13712025b3795d49a00a86630372150b592c7c7f
Enrique Montemayor <eemontemayor@live.com>
2019-05-10 12:23:02 -0500

    working on deploy

--- src/config.js
+++ src/config.js
@@ -3,4 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://capstone1@localhost/capstone1',
   JWT_SECRET: process.env.JWT_SECRET ||'change-this-secret',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:3000/api",
 } 
