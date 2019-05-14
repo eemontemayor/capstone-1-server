a1f71187682531c91a078ddde6dd8d5accdda54e
Enrique Montemayor <eemontemayor@live.com>
2019-05-14 17:20:12 -0500

    x

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   // DB_URL: process.env.DB_URL || 'postgresql://capstone1@localhost/capstone1',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://capstone1@localhost/capstone1',
+  DB_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET ||'change-this-secret',
  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || '*',
 } 
