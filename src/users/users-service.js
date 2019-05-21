61a5e2d8e783f0c0b087fa22185658cba06efb9a
Enrique Montemayor <eemontemayor@live.com>
2019-05-21 13:02:12 -0500

    x

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -18,7 +18,7 @@ const UsersService = {
        },
     validatePassword(password) {
       if (password.length < 8) {
-        return 'Password be longer than 8 characters'
+        return 'Password must be longer than 8 characters and must contain at least: 1 upper case, lower case, number and special character'
       }
       if (password.length > 72) {
         return 'Password be less than 72 characters'
